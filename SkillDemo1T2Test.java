ae83a9f3841e03fcc3df3d6448aea7ef73c676bd
mstubbs1 <103288275+mstubbs1@users.noreply.github.com>
2022-05-01 15:28:42 -0700

    Added new files

---
+++ SkillDemo1T2Test.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemo1T2Test {
+
+	@Test
+	public void subtraction(){
+		assertEquals(1,SkillDemo1.subtract(5,2));
+
+	}
+}
